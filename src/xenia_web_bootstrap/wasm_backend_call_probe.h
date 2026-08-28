19e749fcda2e171191ab7043493366b14f4b09cc
matthewcodergamer <gamerstriper@gmail.com>
2026-08-28 16:32:56 -0400

    Separate Wasm code mutation from mapping invalidation

--- src/xenia_web_bootstrap/wasm_backend_call_probe.h
+++ src/xenia_web_bootstrap/wasm_backend_call_probe.h
@@ -13,7 +13,12 @@ namespace render360::xenia_web {
 void ResetWasmBackendCallProbe();
 bool RegisterWasmBackendCallFunction(xe::cpu::GuestFunction* function,
                                      xe::cpu::hir::HIRBuilder* builder);
+// Mapping/protection invalidation evicts cached code without changing the
+// executable-byte content generation.
 void InvalidateWasmBackendExecutableRange(uint32_t address, uint32_t size);
+// Actual executable-byte mutation advances content generations and evicts code.
+void MarkWasmBackendExecutableContentChangedRange(uint32_t address,
+                                                  uint32_t size);
 uint32_t GetWasmBackendExecutablePageGeneration(uint32_t address);
 uint32_t GetWasmBackendCallStatus();
 uint32_t GetWasmBackendCallFunctionCount();
@@ -46,6 +51,8 @@ uint32_t r360_wasm_backend_call_invalidations();
 uint32_t r360_wasm_backend_executable_page_generation(uint32_t address);
 void r360_wasm_backend_invalidate_executable_range(uint32_t address,
                                                    uint32_t size);
+void r360_wasm_backend_mark_executable_content_changed_range(uint32_t address,
+                                                             uint32_t size);
 }
 
 #endif

298081c9d56d0ccc48d2f921439b033e0a6a77e3
matthewcodergamer <gamerstriper@gmail.com>
2026-08-28 05:16:42 -0400

    Compile generated guest-call WasmBackend workstream

--- build-xenia-ppc-bootstrap.sh
+++ build-xenia-ppc-bootstrap.sh
@@ -68,6 +68,7 @@ compile_one "render360/hir_correctness_executor.cpp" "$OVERLAY/render360/hir_cor
 compile_one "render360/wasm_backend_probe.cpp" "$ROOT/src/xenia_web_bootstrap/wasm_backend_probe.cpp"
 compile_one "render360/wasm_backend_cfg_probe.cpp" "$ROOT/src/xenia_web_bootstrap/wasm_backend_cfg_probe.cpp"
 compile_one "render360/wasm_backend_memory_probe.cpp" "$ROOT/src/xenia_web_bootstrap/wasm_backend_memory_probe.cpp"
+compile_one "render360/wasm_backend_call_probe.cpp" "$ROOT/src/xenia_web_bootstrap/wasm_backend_call_probe.cpp"
 compile_one "render360/probe_backend.cpp" "$ROOT/src/xenia_web_bootstrap/probe_backend.cpp"
 compile_one "render360/ppc_translation_probe.cpp" "$ROOT/src/xenia_web_bootstrap/ppc_translation_probe.cpp"
 compile_one "render360/ppc_context_abi_probe.cpp" "$ROOT/src/xenia_web_bootstrap/ppc_context_abi_probe.cpp"

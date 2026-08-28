3ef75896cbe2f4370a478f745d2302a8b898d3b4
matthewcodergamer <gamerstriper@gmail.com>
2026-08-28 19:48:21 -0400

    V36: add streaming NORMAL framing state

--- src/xenia_web_bootstrap/xex_image_preparer.h
+++ src/xenia_web_bootstrap/xex_image_preparer.h
@@ -16,6 +16,10 @@ enum PrepareStatus : uint32_t {
   kPrepareErrorBasicFormat = 105,
   kPrepareErrorBasicRange = 106,
   kPrepareErrorBasicState = 107,
+  kPrepareErrorNormalFormat = 108,
+  kPrepareErrorNormalRange = 109,
+  kPrepareErrorNormalHash = 110,
+  kPrepareErrorNormalState = 111,
 };
 
 enum PrepareOutputKind : uint32_t {
@@ -29,6 +33,13 @@ constexpr uint32_t kMaxBasicBlocks = 2048;
 struct BasicBlock {
   uint32_t data_size = 0;
   uint32_t zero_size = 0;
+};
+
+struct Sha1State {
+  uint32_t h[5] = {};
+  uint64_t total_bytes = 0;
+  uint8_t buffer[64] = {};
+  uint32_t buffer_bytes = 0;
 };
 
 struct PrepareState {
@@ -45,6 +56,20 @@ struct PrepareState {
   uint32_t basic_block_index = 0;
   uint32_t basic_block_data_done = 0;
   uint32_t basic_block_zero_done = 0;
+
+  uint32_t normal_window_size = 0;
+  uint32_t normal_block_size = 0;
+  uint32_t normal_block_seen = 0;
+  uint32_t normal_blocks_done = 0;
+  uint32_t normal_next_header_seen = 0;
+  uint32_t normal_chunk_length_bytes = 0;
+  uint32_t normal_chunk_length_value = 0;
+  uint32_t normal_chunk_remaining = 0;
+  uint32_t normal_chunk_terminated = 0;
+  uint8_t normal_expected_hash[20] = {};
+  uint8_t normal_next_header[24] = {};
+  Sha1State normal_sha1 = {};
+
   uint32_t last_output_kind = kPrepareOutputNone;
   uint32_t last_output_bytes = 0;
   BasicBlock basic_blocks[kMaxBasicBlocks] = {};
@@ -63,5 +88,14 @@ uint32_t AcceptPrepareBasicData(uint32_t chunk_length, PrepareState* state);
 uint32_t ConsumePrepareBasicZero(uint32_t max_length, PrepareState* state);
 uint32_t PrepareBasicDataRemaining(const PrepareState* state);
 uint32_t PrepareBasicZeroRemaining(const PrepareState* state);
+
+uint32_t BeginPrepareNormalFrame(const uint8_t* staged_header,
+                                 uint32_t staged_length,
+                                 uint32_t file_length,
+                                 ImageMetadata* metadata,
+                                 PrepareState* state);
+uint32_t AcceptPrepareNormalFrameChunk(uint8_t* staged_chunk,
+                                       uint32_t chunk_length,
+                                       PrepareState* state);
 
 }  // namespace render360::xex

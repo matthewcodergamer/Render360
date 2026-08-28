531d6fc1e0d66294fb9487c98cc8e8fefc2c55b8
matthewcodergamer <gamerstriper@gmail.com>
2026-08-28 19:13:12 -0400

    V36: isolate XEX decoder ABI namespace

--- src/xenia_web_bootstrap/xex_image_decoder_exports.cpp
+++ src/xenia_web_bootstrap/xex_image_decoder_exports.cpp
@@ -12,12 +12,12 @@ render360::xex::ImageMetadata metadata;
 extern "C" {
 
 __attribute__((visibility("default")))
-void r360_xex_image_decode_reset() {
+void r360_xex_decode_reset() {
   render360::xex::Reset(&metadata);
 }
 
 __attribute__((visibility("default")))
-uint32_t r360_xex_image_decode(uint32_t length) {
+uint32_t r360_xex_decode(uint32_t length) {
   if (length > r360_io_capacity()) {
     render360::xex::Reset(&metadata);
     metadata.status = render360::xex::kDecodeErrorRange;
@@ -28,56 +28,48 @@ uint32_t r360_xex_image_decode(uint32_t length) {
   return render360::xex::Decode(bytes, length, &metadata);
 }
 
-#define R360_XEX_IMAGE_GETTER(name, field) \
+#define R360_XEX_DECODE_GETTER(name, field) \
   __attribute__((visibility("default"))) uint32_t name() { return metadata.field; }
-R360_XEX_IMAGE_GETTER(r360_xex_image_status, status)
-R360_XEX_IMAGE_GETTER(r360_xex_image_module_flags, module_flags)
-R360_XEX_IMAGE_GETTER(r360_xex_image_header_size, header_size)
-R360_XEX_IMAGE_GETTER(r360_xex_image_security_offset, security_offset)
-R360_XEX_IMAGE_GETTER(r360_xex_image_header_count, header_count)
-R360_XEX_IMAGE_GETTER(r360_xex_image_entry_point, entry_point)
-R360_XEX_IMAGE_GETTER(r360_xex_image_base, image_base)
-R360_XEX_IMAGE_GETTER(r360_xex_image_system_flags, system_flags)
-R360_XEX_IMAGE_GETTER(r360_xex_image_execution_info_offset, execution_info_offset)
-R360_XEX_IMAGE_GETTER(r360_xex_image_file_format_info_offset, file_format_info_offset)
-R360_XEX_IMAGE_GETTER(r360_xex_image_import_libraries_offset, import_libraries_offset)
-R360_XEX_IMAGE_GETTER(r360_xex_image_title_id, title_id)
-R360_XEX_IMAGE_GETTER(r360_xex_image_media_id, media_id)
-R360_XEX_IMAGE_GETTER(r360_xex_image_size, image_size)
-R360_XEX_IMAGE_GETTER(r360_xex_image_flags, image_flags)
-R360_XEX_IMAGE_GETTER(r360_xex_image_load_address, load_address)
-R360_XEX_IMAGE_GETTER(r360_xex_image_region, region)
-R360_XEX_IMAGE_GETTER(r360_xex_image_allowed_media_types, allowed_media_types)
-R360_XEX_IMAGE_GETTER(r360_xex_image_encryption_type, encryption_type)
-R360_XEX_IMAGE_GETTER(r360_xex_image_compression_type, compression_type)
-R360_XEX_IMAGE_GETTER(r360_xex_image_page_size, page_size)
-R360_XEX_IMAGE_GETTER(r360_xex_image_page_descriptor_count, page_descriptor_count)
-R360_XEX_IMAGE_GETTER(r360_xex_image_mapped_span, mapped_span)
-#undef R360_XEX_IMAGE_GETTER
+R360_XEX_DECODE_GETTER(r360_xex_decode_status, status)
+R360_XEX_DECODE_GETTER(r360_xex_decode_module_flags, module_flags)
+R360_XEX_DECODE_GETTER(r360_xex_decode_header_size, header_size)
+R360_XEX_DECODE_GETTER(r360_xex_decode_security_offset, security_offset)
+R360_XEX_DECODE_GETTER(r360_xex_decode_header_count, header_count)
+R360_XEX_DECODE_GETTER(r360_xex_decode_entry_point, entry_point)
+R360_XEX_DECODE_GETTER(r360_xex_decode_image_base, image_base)
+R360_XEX_DECODE_GETTER(r360_xex_decode_system_flags, system_flags)
+R360_XEX_DECODE_GETTER(r360_xex_decode_execution_info_offset, execution_info_offset)
+R360_XEX_DECODE_GETTER(r360_xex_decode_file_format_info_offset, file_format_info_offset)
+R360_XEX_DECODE_GETTER(r360_xex_decode_import_libraries_offset, import_libraries_offset)
+R360_XEX_DECODE_GETTER(r360_xex_decode_title_id, title_id)
+R360_XEX_DECODE_GETTER(r360_xex_decode_media_id, media_id)
+R360_XEX_DECODE_GETTER(r360_xex_decode_image_size, image_size)
+R360_XEX_DECODE_GETTER(r360_xex_decode_image_flags, image_flags)
+R360_XEX_DECODE_GETTER(r360_xex_decode_load_address, load_address)
+R360_XEX_DECODE_GETTER(r360_xex_decode_region, region)
+R360_XEX_DECODE_GETTER(r360_xex_decode_allowed_media_types, allowed_media_types)
+R360_XEX_DECODE_GETTER(r360_xex_decode_encryption_type, encryption_type)
+R360_XEX_DECODE_GETTER(r360_xex_decode_compression_type, compression_type)
+R360_XEX_DECODE_GETTER(r360_xex_decode_page_size, page_size)
+R360_XEX_DECODE_GETTER(r360_xex_decode_page_descriptor_count, page_descriptor_count)
+R360_XEX_DECODE_GETTER(r360_xex_decode_mapped_span, mapped_span)
+#undef R360_XEX_DECODE_GETTER
 
 __attribute__((visibility("default")))
-uint32_t r360_xex_image_page_type(uint32_t index) {
-  return index < metadata.page_descriptor_count
-             ? metadata.page_descriptors[index].type
-             : 0u;
+uint32_t r360_xex_decode_page_type(uint32_t index) {
+  return index < metadata.page_descriptor_count ? metadata.page_descriptors[index].type : 0u;
 }
 __attribute__((visibility("default")))
-uint32_t r360_xex_image_page_count(uint32_t index) {
-  return index < metadata.page_descriptor_count
-             ? metadata.page_descriptors[index].page_count
-             : 0u;
+uint32_t r360_xex_decode_page_count(uint32_t index) {
+  return index < metadata.page_descriptor_count ? metadata.page_descriptors[index].page_count : 0u;
 }
 __attribute__((visibility("default")))
-uint32_t r360_xex_image_page_address(uint32_t index) {
-  return index < metadata.page_descriptor_count
-             ? metadata.page_descriptors[index].guest_address
-             : 0u;
+uint32_t r360_xex_decode_page_address(uint32_t index) {
+  return index < metadata.page_descriptor_count ? metadata.page_descriptors[index].guest_address : 0u;
 }
 __attribute__((visibility("default")))
-uint32_t r360_xex_image_page_bytes(uint32_t index) {
-  return index < metadata.page_descriptor_count
-             ? metadata.page_descriptors[index].byte_size
-             : 0u;
+uint32_t r360_xex_decode_page_bytes(uint32_t index) {
+  return index < metadata.page_descriptor_count ? metadata.page_descriptors[index].byte_size : 0u;
 }
 
 }  // extern "C"

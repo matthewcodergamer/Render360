8edc3579d6a2d94436f4910b76e2eae7de680698
matthewcodergamer <gamerstriper@gmail.com>
2026-08-28 21:33:18 -0400

    V36: define prepared PE guest-load contract

---
+++ src/xenia_web_bootstrap/xex_pe_guest_loader.h
@@ -0,0 +1,39 @@
+#ifndef RENDER360_XENIA_WEB_BOOTSTRAP_XEX_PE_GUEST_LOADER_H_
+#define RENDER360_XENIA_WEB_BOOTSTRAP_XEX_PE_GUEST_LOADER_H_
+
+#include <cstdint>
+
+namespace render360::xenia_web {
+
+enum PeGuestLoadStatus : uint32_t {
+  kPeGuestIdle = 0,
+  kPeGuestPass = 1,
+  kPeGuestInvalidArgument = 0x81000001u,
+  kPeGuestDecodeFailed = 0x81000002u,
+  kPeGuestAddressOverflow = 0x81000003u,
+  kPeGuestProtectionInvalid = 0x81000004u,
+  kPeGuestMapFailed = 0x81000005u,
+  kPeGuestLoadFailed = 0x81000006u,
+  kPeGuestEntryFailed = 0x81000007u,
+  kPeGuestFinalizeFailed = 0x81000008u,
+};
+
+void ResetPreparedPeGuestLoad();
+bool LoadPreparedPeImageToGuest(const uint8_t* image, uint32_t length);
+uint32_t PreparedPeGuestLoadStatus();
+uint32_t PreparedPeGuestEntryAddress();
+uint32_t PreparedPeGuestSectionCount();
+uint32_t PreparedPeGuestRawBytes();
+
+}  // namespace render360::xenia_web
+
+extern "C" {
+void r360_pe_guest_reset();
+uint32_t r360_pe_guest_load(uint32_t source_ptr, uint32_t length);
+uint32_t r360_pe_guest_status();
+uint32_t r360_pe_guest_entry_address();
+uint32_t r360_pe_guest_section_count();
+uint32_t r360_pe_guest_raw_bytes();
+}
+
+#endif

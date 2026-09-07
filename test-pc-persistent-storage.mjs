aa571ba5d55eac44ff968f106dd3078895a6ceca
matthewcodergamer <gamerstriper@gmail.com>
2026-09-06 22:14:20 -0400

    Portal: test stale runtime rejection in persistent storage

--- test-pc-persistent-storage.mjs
+++ test-pc-persistent-storage.mjs
@@ -10,6 +10,11 @@ assert.match(storage,/getDirectory/);
 assert.match(storage,/Render360/);
 assert.match(storage,/const PC_DIR='PC'/);
 assert.match(storage,/COPY_CHUNK=4\*1024\*1024/);
+assert.match(storage,/REQUIRED_PORTAL_STACK_REPAIR_VERSION=4/);
+assert.match(storage,/assertFreshPortalRuntime/);
+assert.match(storage,/runtimeStackRepairVersion/);
+assert.match(storage,/runtimeProfile/);
+assert.match(storage,/Relink using the newest Portal runtime ZIP/);
 assert.match(storage,/persistPcRecompiledSource/);
 assert.match(storage,/restorePcRecompiledSource/);
 assert.match(storage,/loadCommunityWasmPackageFromFiles/);
@@ -32,4 +37,5 @@ assert.match(ui,/sourceType:'pc-wasm'/);
 
 console.log('PC_OPFS_PERSISTENT_GAME_FILES=PASS');
 console.log('PC_OPFS_PERSISTENT_RUNTIME_PACKAGE=PASS');
+console.log('PC_OPFS_STALE_PORTAL_RUNTIME_REJECT=PASS');
 console.log('PC_OPFS_AUTOMATIC_RESTORE_WITHOUT_PICKER=PASS');
